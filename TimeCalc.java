017b6254e6bce45c67b67fef8e1f3aa9820baa02
bar.kalkstein <bar.kalkstein@post.runi.ac.il>
2025-11-13 10:21:45 +0200

    bar

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class TimeCalc {
     public static void main(String[] args) {
         // Replace this comment with your code
@@ -22,10 +21,3 @@ public class TimeCalc {
         System.out.println(result);
 	}
 }
-=======
-public class TimeCalc {
-    public static void main(String[] args) {
-        // Replace this comment with your code
-    }
-}
->>>>>>> f5de349671672dfb71c17b605b2edaaaaacd06a6
